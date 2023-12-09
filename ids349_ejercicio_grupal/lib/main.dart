0c86369cb115881ebe77c41837927b643be1244e
Huan Hao Wu <huanhaowu28@gmail.com>
2023-12-08 23:57:15 -0400

    Agregar seccion meteorologica
    Se agrego el widget de seccion meteorologica y se añadieron cambios al layout

--- ids349_ejercicio_grupal/lib/main.dart
+++ ids349_ejercicio_grupal/lib/main.dart
@@ -1,43 +1,77 @@
 import 'package:flutter/material.dart';
 import 'diary_app_theme.dart';
+import 'meteo_section.dart';
 
 void main() => runApp(DiaryApp());
 
 class DiaryApp extends StatelessWidget {
+  final title = "My Birthday";
+  final text =
+      "It's going to be a great birthday. We are going out for dinner at my favorite place, then watch a movie afer we go to the gelateria ofr ice cream and espresso";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Diary App',
-      theme: DiaryAppTheme.lightTheme(),
-      home: Scaffold(
-        appBar: AppBar(),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset("assets/regalos.jpg"),
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0, top: 10.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Titulo",
-                        style: DiaryAppTheme.lightTextTheme.displayLarge,
-                      ),
-                      Text(
-                        "Descripcion",
-                        style: DiaryAppTheme.lightTextTheme.displaySmall,
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ],
+        debugShowCheckedModeBanner: false,
+        title: 'Diary App',
+        theme: DiaryAppTheme.lightTheme(),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              children: [
+                Icon(Icons.menu),
+                Spacer(),
+                Text("Layouts"),
+                Spacer(),
+                Icon(Icons.cloud_outlined),
+              ],
+            ),
           ),
-        ),
-      )
-    );
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset("assets/regalos.jpg"),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                          child: Text(
+                            title,
+                            style: DiaryAppTheme.lightTextTheme.displayLarge,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                          child: Text(
+                            text,
+                            style: TextStyle(color: Colors.grey, fontSize: 18),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                          child: MeteoSection(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Placeholder(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Placeholder(),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
